5004cec9d2115b5416a2f0128bd5a611d7051aec
Ioannis Katergaris (devops cloud VM) <jkatergaris@gmail.com>
2025-03-31 20:47:26 +0300

    Reverted attempt to change to catppuccin

--- init.lua
+++ init.lua
@@ -897,6 +897,8 @@ require('lazy').setup({
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
     'folke/tokyonight.nvim',
+    -- 'catppuccin/nvim',
+    name = 'tokyonight',
     priority = 1000, -- Make sure to load this before all the other start plugins.
     config = function()
       ---@diagnostic disable-next-line: missing-fields
@@ -905,11 +907,10 @@ require('lazy').setup({
           comments = { italic = false }, -- Disable italics in comments
         },
       }
-
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-storm'
+      vim.cmd.colorscheme 'tokyonight'
     end,
   },
 
